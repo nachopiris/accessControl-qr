e1745412c2512b49e7200910ff48ad6f7ffd9dbb
Ignacio <ignaciopirisb@gmail.com>
2022-03-31 00:30:14 -0300

    fix 7 i want to finish please

--- scripts/addRrpps.js
+++ scripts/addRrpps.js
@@ -8,14 +8,29 @@ const main = async () => {
     const rrpps = getRrpps();
     for (let i = 0; i < rrpps.length; i++) {
       const { fullName, dni, phoneNumber } = rrpps[i];
+      let rrpp = await prisma.rrpp.findUnique({
+        where: {
+          dni
+        }
+      })
       const guest = await prisma.guest.findUnique({
         where: {
           dni
         }
       })
+      if (!rrpp) {
+        rrpp = await prisma.rrpp.create({
+          data: {
+            fullName,
+            dni,
+            phoneNumber,
+          },
+        });
+      }
       if (!guest) {
         await prisma.guest.create({
           data: {
+            rrpp_id: rrpp.id,
             fullName,
             dni,
             phoneNumber,
